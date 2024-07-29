eddaf9c238202f75cb553487e5ff36749dbc267d
Natnael Wondwoesn Solomon <ninatina423@gmail.com>
2024-07-29 13:35:54 +0300

    First Commit

---
+++ on-boarding/product_2/product_2.dart
@@ -0,0 +1,27 @@
+void main(){
+  // var name = "Jhon";
+  // print(name);
+  //Data Types
+  /* Nums,Strs,Booleans,Lists,Maps,Dynamic
+  Strings = */
+   
+
+}
+// Class decleration
+class Product{
+  String? name;
+  String? description;
+  int? price;
+
+  // Constructors
+  Product(String name, description,int price){
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+
+}
+
+class productManager{
+  
+}
